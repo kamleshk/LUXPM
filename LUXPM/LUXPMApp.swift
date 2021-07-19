ac3a92972540448ef1d899da63e7a384781a80db
kamalesh <kamalesh.yadav@lycadigital.com>
2021-07-19 22:53:08 +0530

    Initial Commit

---
+++ LUXPM/LUXPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  LUXPMApp.swift
+//  LUXPM
+//
+//  Created by kamlesh kumar on 19/07/21.
+//
+
+import SwiftUI
+
+@main
+struct LUXPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
