2ecbe22f143b28c29250a4b751aedfd93ec81c2c
kamalesh <kamalesh.yadav@lycadigital.com>
2021-07-20 20:20:11 +0530

    adding test model for login, testing funcanality

---
+++ LUXPMTests/LoginViewModelTest.swift
@@ -0,0 +1,49 @@
+//
+//  LoginViewModelTest.swift
+//  LUXPMTests
+//
+//  Created by kamlesh kumar on 20/07/21.
+//
+
+import XCTest
+@testable import LUXPM
+
+class LoginViewModelTest: XCTestCase {
+var loginModel = LoginViewModel()
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+	func testEmail()  {
+		XCTAssertFalse(loginModel.isValidEmail(email: "abc"))
+	}
+	
+	func testValidEmail()  {
+		XCTAssertTrue(loginModel.isValidEmail(email: "abc@gamil.com"))
+	}
+	
+	func testValidString()  {
+		loginModel.email = "kamalesh@gmail.com"
+		XCTAssertEqual(loginModel.email, "kamalesh@gmail.com")
+	}
+	func testvalitedStr()  {
+		loginModel.email = ""
+		XCTAssertEqual(loginModel.isloginSuccess(), "Please provide valid email address")
+	}
+	
+}
